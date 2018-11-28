2e8a7039d8e1c747e6be1b459bbb49d8c8e49077
evalor <master@evalor.cn>
2018-11-28 21:16:11 +0800

    release dev

--- App/WebSocket/WebSocketEvents.php
+++ App/WebSocket/WebSocketEvents.php
@@ -8,7 +8,65 @@
 
 namespace App\WebSocket;
 
+use App\Utility\AppConst;
+use App\Utility\Pool\RedisPool;
+use App\Utility\Pool\RedisPoolObject;
+use EasySwoole\Component\Pool\PoolManager;
+
 class WebSocketEvents
 {
+    /**
+     * 链接打开时 将用户的FD存入Redis
+     * @param \swoole_server       $server
+     * @param \swoole_http_request $req
+     * @throws \Exception
+     */
+    static function onOpen(\swoole_server $server, \swoole_http_request $req)
+    {
+        $redisPool = PoolManager::getInstance()->getPool(RedisPool::class);
+        $redis = $redisPool->getObj();
+        if ($redis instanceof RedisPoolObject) {
+            echo "websocket user {$req->fd} was connected\n";
+            $redis->hSet(AppConst::REDIS_ONLINE_KEY, $req->fd, false);
+            $redisPool->recycleObj($redis);
+        } else {
+            throw new \Exception('redis pool is empty');
+        }
+    }
 
+    /**
+     * 链接关闭时 将用户的FD从Redis删除
+     * @param \swoole_server $server
+     * @param int            $fd
+     * @param int            $reactorId
+     * @throws \Exception
+     */
+    static function onClose(\swoole_server $server, int $fd, int $reactorId)
+    {
+        $redisPool = PoolManager::getInstance()->getPool(RedisPool::class);
+        $redis = $redisPool->getObj();
+        if ($redis instanceof RedisPoolObject) {
+            echo "websocket user {$fd} was close\n";
+            $redis->hDel(AppConst::REDIS_ONLINE_KEY, $fd);
+            $redisPool->recycleObj($redis);
+        } else {
+            throw new \Exception('redis pool is empty');
+        }
+    }
+
+    /**
+     * 清空在线用户列表
+     * @throws \Exception
+     */
+    static function cleanOnlineUser()
+    {
+        $redisPool = PoolManager::getInstance()->getPool(RedisPool::class);
+        $redis = $redisPool->getObj();
+        if ($redis instanceof RedisPoolObject) {
+            $redis->del(AppConst::REDIS_ONLINE_KEY);
+            $redisPool->recycleObj($redis);
+        } else {
+            throw new \Exception('redis pool is empty');
+        }
+    }
 }
--- EasySwooleEvent.php
+++ EasySwooleEvent.php
@@ -8,40 +8,68 @@
 
 namespace EasySwoole\EasySwoole;
 
-
 use App\Utility\Pool\RedisPool;
+use App\WebSocket\WebSocketEvents;
+use App\WebSocket\WebSocketParser;
 use EasySwoole\Component\Pool\PoolManager;
 use EasySwoole\EasySwoole\AbstractInterface\Event;
 use EasySwoole\EasySwoole\Swoole\EventRegister;
 use EasySwoole\Http\Request;
 use EasySwoole\Http\Response;
+use EasySwoole\Socket\Dispatcher;
 
 class EasySwooleEvent implements Event
 {
 
     public static function initialize()
     {
-        // TODO: Implement initialize() method.
         date_default_timezone_set('Asia/Shanghai');
     }
 
+    /**
+     * 服务启动前事件
+     * @param EventRegister $register
+     * @throws \Exception
+     */
     public static function mainServerCreate(EventRegister $register)
     {
+        // 注册链接池
         PoolManager::getInstance()->register(RedisPool::class, 10);
+
+        // 注册WS事件回调
+        $conf = new \EasySwoole\Socket\Config();
+        $conf->setType($conf::WEB_SOCKET);
+        $conf->setParser(new WebSocketParser);
+        $dispatch = new Dispatcher($conf);
+
+        // 收到客户端消息时的处理
+        $register->set(EventRegister::onMessage, function (\swoole_websocket_server $server, \swoole_websocket_frame $frame) use ($dispatch) {
+            $dispatch->dispatch($server, $frame->data, $frame);
+        });
+
+        // 链接打开和关闭时的处理
+        $register->set(EventRegister::onOpen, [WebSocketEvents::class, 'onOpen']);
+        $register->set(EventRegister::onClose, [WebSocketEvents::class, 'onClose']);
+
+        // 启动时清理 在线用户列表直接清空
+        $register->add($register::onWorkerStart, function (\swoole_websocket_server $server, $workerId) {
+            if ($workerId == 0) {
+                WebSocketEvents::cleanOnlineUser();
+            }
+        });
     }
 
     public static function onRequest(Request $request, Response $response): bool
     {
-        // TODO: Implement onRequest() method.
         return true;
     }
 
     public static function afterRequest(Request $request, Response $response): void
     {
-        // TODO: Implement afterAction() method.
+
     }
 
-    public static function onReceive(\swoole_server $server, int $fd, int $reactor_id, string $data):void
+    public static function onReceive(\swoole_server $server, int $fd, int $reactor_id, string $data): void
     {
 
     }
